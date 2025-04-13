2a7642c6b0819e1e67e95c392bd7ef20f55a2f85
Amy Varga <amyvarga@Amys-MacBook-Pro.local>
2025-04-13 21:22:35 +0100

    Canvas circles and rectangle

--- src/Canvas/Canvas.js
+++ src/Canvas/Canvas.js
@@ -4,13 +4,48 @@ function Canvas() {
   const canvasRef = useRef(null);
   useEffect(() => {
     const canvas = canvasRef.current;
+    canvas.width = window.innerWidth - 10;
+    canvas.height = window.innerHeight- 10;
+    canvas.midWidth = canvas.width/2;
+    canvas.midHeight = canvas.height/2;
+    const circleRadius = canvas.width*0.15;
 
-    const context = canvas.getContext('2d');
+    const ctx = canvas.getContext('2d');
+    ctx.lineWidth=1;
 
-    // Start drawing
-    context.beginPath();
-    context.arc(110,100,40,0,2*Math.PI);
-    context.stroke();
+    //circles
+    ctx.beginPath();
+    ctx.arc(canvas.midWidth, canvas.midHeight, circleRadius, 0, 2*Math.PI);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.arc(canvas.midWidth+circleRadius, canvas.midHeight, circleRadius, 0, 2*Math.PI);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.arc(canvas.midWidth-circleRadius, canvas.midHeight, circleRadius, 0, 2*Math.PI);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.arc(canvas.midWidth, canvas.midHeight+circleRadius, circleRadius, 0, 2*Math.PI);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.arc(canvas.midWidth, canvas.midHeight-circleRadius, circleRadius, 0, 2*Math.PI);
+    ctx.stroke();
+
+    //rectangle
+    ctx.beginPath();
+    ctx.moveTo(canvas.midWidth-circleRadius, canvas.midHeight-circleRadius);
+    ctx.lineTo(canvas.midWidth-circleRadius, canvas.midHeight+circleRadius);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(canvas.midWidth+circleRadius, canvas.midHeight-circleRadius);
+    ctx.lineTo(canvas.midWidth+circleRadius, canvas.midHeight+circleRadius);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(canvas.midWidth-circleRadius, canvas.midHeight-circleRadius);
+    ctx.lineTo(canvas.midWidth+circleRadius, canvas.midHeight-circleRadius);
+    ctx.stroke();
+    ctx.moveTo(canvas.midWidth+circleRadius, canvas.midHeight+circleRadius);
+    ctx.lineTo(canvas.midWidth-circleRadius, canvas.midHeight+circleRadius);
+    ctx.stroke();
 
   }, []);
 
